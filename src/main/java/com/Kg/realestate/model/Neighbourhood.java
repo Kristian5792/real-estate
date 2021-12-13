ae69c1da6304656c656447e5cd1da4b0b2701556
Kristian5792 <kris_georgiev@abv.bg>
2021-12-13 12:34:56 +0200

    Add floor and neighbourhood entity

---
+++ src/main/java/com/Kg/realestate/model/Neighbourhood.java
@@ -0,0 +1,27 @@
+package com.Kg.realestate.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
+@Getter
+@Entity
+@Table(name = "neighbourhoods")
+
+public class Neighbourhood {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @NotNull
+    @Column(nullable = false, unique = true)
+    private String name;
+}
